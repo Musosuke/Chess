643323b8add50fd501e8457176d73b28070b5f8a
In <53400743+Musosuke@users.noreply.github.com>
2023-05-17 13:24:33 +0800

    update : implement Chess.FindMoveList

--- MineSweeper.GUI.DearImgui/Chess.cpp
+++ MineSweeper.GUI.DearImgui/Chess.cpp
@@ -8,7 +8,13 @@ void Chess::AddMovelist(int y, int x) {
 	Movelist->push_back(make_pair(y, x));
 }
 
-bool Chess::FindMovelist(int, int) {
-
+bool Chess::FindMovelist(int y, int x) {
+	//find da pair exist or not
+	std::vector<std::pair<int, int>>::iterator it;
+	for (it = Movelist->begin(); it != Movelist->end(); it++) {
+		if (it->first == y && it->second == x)
+			return true;
+	}
+	return false;
 }
 
